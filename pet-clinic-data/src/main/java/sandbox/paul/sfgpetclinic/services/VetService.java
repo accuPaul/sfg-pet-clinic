400b1365e6d80a6675b3e514331527d5337ce710
paul <paul.mount@gmail.com>
2020-11-18 14:29:15 -0500

    Implement Common BaseService interface.

--- pet-clinic-data/src/main/java/sandbox/paul/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/sandbox/paul/sfgpetclinic/services/VetService.java
@@ -2,14 +2,7 @@ package sandbox.paul.sfgpetclinic.services;
 
 import sandbox.paul.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
+public interface VetService extends CrudService<Vet, Long>{
 
     Vet findByLastName(String lastName);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
