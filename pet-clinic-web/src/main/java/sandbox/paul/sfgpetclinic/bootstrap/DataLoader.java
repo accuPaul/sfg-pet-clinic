2fe32e0d5318fc3e3c9f682a9f27cc612fd4a69d
paul <paul.mount@gmail.com>
2020-11-19 16:52:08 -0500

    Auto-generate ID in the AbstractMapService Closes #21.
    This is based on the HashMap size.

--- pet-clinic-web/src/main/java/sandbox/paul/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sandbox/paul/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,12 +23,10 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId((1L));
         owner1.setFirstName("Lucy");
         owner1.setLastName("Gordon");
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Paul");
         owner2.setLastName("Mount");
 
@@ -38,12 +36,10 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners loaded...");
 
         Vet vet1 = new Vet();
-        vet1.setId((1L));
         vet1.setFirstName("Jessie");
         vet1.setLastName("Yellowlab");
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Bennett");
         vet2.setLastName("Blacklab");
 
