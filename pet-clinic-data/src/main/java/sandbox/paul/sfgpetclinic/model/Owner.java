6a2b484f5838e6f8a377bebb4864c20f6cdca9ea
paul <paul.mount@gmail.com>
2020-11-24 15:39:47 -0500

    Added address, city, and phone number to Owners. Closes #26

--- pet-clinic-data/src/main/java/sandbox/paul/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/sandbox/paul/sfgpetclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String City;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
