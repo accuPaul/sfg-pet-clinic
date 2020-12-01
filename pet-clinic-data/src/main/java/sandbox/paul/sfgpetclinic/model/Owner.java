fddcc490cb8c478842d24d116c0ece4727f1218d
paul <paul.mount@gmail.com>
2020-12-01 11:50:14 -0500

    Finished mappings for Owner JPA entity. Closes #34
    Also modified Person, Pets, and PetType

--- pet-clinic-data/src/main/java/sandbox/paul/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/sandbox/paul/sfgpetclinic/model/Owner.java
@@ -1,13 +1,20 @@
 package sandbox.paul.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
 
+    @Column(name="address")
     private String address;
+    @Column(name="City")
     private String City;
+    @Column(name="telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
@@ -39,6 +46,7 @@ public class Owner extends Person {
     }
 
     public void setPets(Set<Pet> pets) {
+        for (Pet pet : pets) { pet.setOwner(this); }
         this.pets = pets;
     }
 }
